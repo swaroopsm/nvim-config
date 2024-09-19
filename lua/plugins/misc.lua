f5d3daf8e6773e4f68d7be7afab0b6c67dae1244
Swaroop Sethumadhavan <swaroop.striker@gmail.com>
2024-09-19 15:44:41 +0200

    Configure for http rest

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -9,4 +9,28 @@ return {
       colorscheme = "ayu-dark",
     },
   },
+
+  {
+    "mistweaverco/kulala.nvim",
+    opts = {
+      contenttypes = {
+        ["application/json"] = {
+          ft = "json",
+          formatter = { "jq", "." },
+          pathresolver = require("kulala.parser.jsonpath").parse,
+        },
+      },
+    },
+  },
+
+  {
+    "nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+    },
+  },
+
+  {
+    "benfowler/telescope-luasnip.nvim",
+  },
 }
